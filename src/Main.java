635b4dbf1afdbef7568e6df9816307a2ed42bf8c
gssh1991 <gssh1991@gmail.com>
2020-09-02 01:08:35 +0300

    Добавил переменную и исправил приветствие

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+
+    String s = "X5";
+
     public static void main(String[] args){
-        System.out.println("HelloWorld!");
+        System.out.println("Hello" + s + "!");
     }
 }
